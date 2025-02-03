0dabcfdb0f356f9bff9e2e25c853872e9489379d
Buumty <wojtekand22@gmail.com>
2025-02-03 19:46:26 +0100

    first commit

---
+++ src/main/java/pl/wojciechandrzejczak/cinema_ticket_reservation_app/CinemaTicketReservationAppApplication.java
@@ -0,0 +1,13 @@
+package pl.wojciechandrzejczak.cinema_ticket_reservation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketReservationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketReservationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wojciechandrzejczak/cinema_ticket_reservation_app/CinemaTicketReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojciechandrzejczak.cinema_ticket_reservation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketReservationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
