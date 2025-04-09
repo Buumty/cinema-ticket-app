d1dba9177f54ee8be09ee2d20a2e79e5119c22ea
Buumty <wojtekand22@gmail.com>
2025-04-09 21:24:46 +0200

    Adding links to Admin and User panels

--- src/main/java/pl/wojciechandrzejczak/cinema_ticket_reservation_app/views/components/AdminFooter.java
+++ src/main/java/pl/wojciechandrzejczak/cinema_ticket_reservation_app/views/components/AdminFooter.java
@@ -2,6 +2,8 @@ package pl.wojciechandrzejczak.cinema_ticket_reservation_app.views.components;
 
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.RouterLink;
+import pl.wojciechandrzejczak.cinema_ticket_reservation_app.views.userviews.MainView;
 
 public class AdminFooter extends HorizontalLayout {
     public AdminFooter() {
@@ -10,5 +12,6 @@ public class AdminFooter extends HorizontalLayout {
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
         add(new Span("Admin Footer"));
+        add(new RouterLink("User Panel", MainView.class));
     }
 }
--- src/main/java/pl/wojciechandrzejczak/cinema_ticket_reservation_app/views/components/UserFooter.java
+++ src/main/java/pl/wojciechandrzejczak/cinema_ticket_reservation_app/views/components/UserFooter.java
@@ -3,6 +3,8 @@ package pl.wojciechandrzejczak.cinema_ticket_reservation_app.views.components;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.RouterLink;
+import pl.wojciechandrzejczak.cinema_ticket_reservation_app.views.adminviews.MovieView;
 
 public class UserFooter extends HorizontalLayout {
     public UserFooter() {
@@ -11,5 +13,6 @@ public class UserFooter extends HorizontalLayout {
         setAlignItems(FlexComponent.Alignment.CENTER);
         setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
         add(new Span("User Footer"));
+        add(new RouterLink("Admin Panel", MovieView.class));
     }
 }
